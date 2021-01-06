5a8907b20e0d43e9c6338667bf6802722740e30d
cleristonnovaes <novaes.cleriston@gmail.com>
2021-01-06 18:12:56 -0300

    Aplicacao prática do conteudo

---
+++ Aluno.cs
@@ -0,0 +1,14 @@
+namespace First_Csharp
+{
+    internal class Aluno
+    {
+    }
+}namespace 
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; }
+
+        public decimal Nota { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,12 +6,44 @@ namespace First_Csharp
     {
         static void Main(string[] args)
         {
-            int numeroDeVezes = 10;
+            Aluno[] alunos = new Aluno[5];
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-            for(int i=1; i <= numeroDeVezes; i++){
-                Console.WriteLine($"Hello World {i}");
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                        break;
+                    case "2":
+                        //Listar alunos
+                        break;
+                    case "3":
+                        //Calcular média geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+            opcaoUsuario = ObterOpcaoUsuario();
             }
-            
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar aluno");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
